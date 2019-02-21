15ae080cbb24d5d7f4a0f835c33027156d96a2fa
John Karvonen <jpkarvonen@gmail.com>
2019-02-21 11:25:50 -0500

    implement touch & mdkir

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const terminalCommands = require('./terminal-commands');
 
+console.log("running ls command");
 terminalCommands.ls();
+
+console.log("running touch command");
+terminalCommands.touch('hello mom.txt', 'Hi mom!');
+
+console.log("running mkdir command");
+terminalCommands.mkdir('./tmp/test');
--- terminal-commands.js
+++ terminal-commands.js
@@ -9,3 +9,19 @@ module.exports.ls = () => {
     console.log(filesToString);
   });
 };
+
+module.exports.touch = (file, content) => {
+  fs.writeFile(file, content, 'utf8', (err) => {
+    if (err) throw err;
+
+    console.log('file successfully created!')
+  });
+};
+
+module.exports.mkdir = (file_path) => {
+  fs.mkdir(file_path, { recursive: true }, (err) => {
+    if (err) throw err;
+
+    console.log('directory successfully created!')
+  });
+};
